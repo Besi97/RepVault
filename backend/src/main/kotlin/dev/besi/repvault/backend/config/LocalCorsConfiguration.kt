ae648f0bc6b206411f8a3ddf40defedbd0caeec2
Dávid Besenyei <david.besenyei97@gmail.com>
2025-04-26 10:39:51 +0200

    Add CORS configuration for local development (#23)
    This change enables CORS for a local development environment, allowing interactions between the backend and a frontend hosted on localhost:3000. It ensures seamless integration and testing during development by permitting all methods, headers, and credentials.
    
    Reviewed-on: https://git.besi.dev/besi/RepVault/pulls/23
    Co-authored-by: Dávid Besenyei <david.besenyei97@gmail.com>
    Co-committed-by: Dávid Besenyei <david.besenyei97@gmail.com>

---
+++ backend/src/main/kotlin/dev/besi/repvault/backend/config/LocalCorsConfiguration.kt
@@ -0,0 +1,25 @@
+package dev.besi.repvault.backend.config
+
+import org.jetbrains.annotations.NotNull
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@Profile("local")
+class LocalCorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(@NotNull registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
